71abe757b8871cda79584b341779c85e52d19e12
Kevin Jacoby <kvnjcby@gmail.com>
2017-12-05 02:57:52 -0500

    Ordering

--- app/src/main/java/com/kejaco/androidorderup/MainActivity.java
+++ app/src/main/java/com/kejaco/androidorderup/MainActivity.java
@@ -1,13 +1,19 @@
 package com.kejaco.androidorderup;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
         // Construct the data source
         ArrayList<MenuItem> arrayOfUsers = new ArrayList<MenuItem>();
         // Create the adapter to convert the array to views
@@ -29,13 +37,15 @@ public class MainActivity extends AppCompatActivity {
         listView.setAdapter(adapter);
 
 
-        MenuItem newItem = new MenuItem("Nachos", "yummy", "6.00", "/nophoto");
+        MenuItem newItem = new MenuItem("Nachos", "yummy", "6.01", "/nophoto");
         adapter.add(newItem);
-        newItem = new MenuItem("Pizza", "circular", "10.00", "/nophoto");
+        newItem = new MenuItem("Pizza", "circular", "10.10", "/nophoto");
         adapter.add(newItem);
-        newItem = new MenuItem("PBR", "healthy", "4.00", "/nophoto");
+        newItem = new MenuItem("PBR", "healthy", "4.02", "/nophoto");
         adapter.add(newItem);
+        newItem = new MenuItem("Cheese", "milk + bacteria = cheese", "33.33", "/nophoto");
         adapter.add(newItem);
+        newItem = new MenuItem("Fruit loops", "sweetened, fruit-flavored breakfast cereal", "9.99", "/nophoto");
         adapter.add(newItem);
         adapter.add(newItem);
         adapter.add(newItem);
@@ -51,10 +61,48 @@ public class MainActivity extends AppCompatActivity {
         orderItems.add(name);
 
         TextView totalView = (TextView) findViewById(R.id.orderTotal);
-        totalView.setText("$"+orderTotal);
+        NumberFormat cF = NumberFormat.getCurrencyInstance();
+        totalView.setText(cF.format(orderTotal));
     }
 
     public void placeOrder(View v) {
+        EditText nameField = (EditText) findViewById(R.id.custName);
+        String custName = nameField.getText().toString();
 
+        String items = "";
+
+        for(String item : orderItems) {
+            if (items.length() > 0) {
+                items = items + ", ";
+            }
+            items = items + item;
+        }
+
+
+        Order newOrder = new Order(custName, orderTotal+"", items, 0);
+        OrderActivity.addOrder(newOrder);
+
+        orderTotal = 0;
+        orderItems.clear();
+        nameField.setText("");
+
+        TextView totalView = (TextView) findViewById(R.id.orderTotal);
+        totalView.setText("$0.00");
+
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Order Placed!");
+        alertDialog.setMessage("Congratulations! Your order has been placed. Please pay.");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Okay!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+
+    public void viewOrders(View v) {
+        Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/kejaco/androidorderup/OrderActivity.java
+++ app/src/main/java/com/kejaco/androidorderup/OrderActivity.java
@@ -3,11 +3,19 @@ package com.kejaco.androidorderup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class OrderActivity extends AppCompatActivity {
+
+    protected static ArrayList<Order> orders = new ArrayList<Order>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+    }
+
+    public static void addOrder(Order order) {
+        orders.add(order);
     }
 }
